378bbb6498076a6ab78c7c99f58f260216a147be
darthjee <darthjee@gmail.com>
2021-10-23 21:56:41 +0200

    Check commit only on some cases

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,38 +10,48 @@ function isLatestCommit() {
   fi
 }
 
-if $(isLatestCommit); then
-  echo "latest commit";
-else
-  echo "Not last commit"
-  exit 0
-fi
+function checkLastCommit() {
+  if $(isLatestCommit); then
+    echo "latest commit";
+  else
+    echo "Not last commit"
+    exit 0
+  fi
+}
 
 ACTION=$1
 
 case $ACTION in
   "install")
+    checkLastCommit
     curl https://cli-assets.heroku.com/install.sh | sh
     ;;
   "set-app")
+    checkLastCommit
     heroku git:remote -a $HEROKU_APP_NAME
     ;;
   "signin")
+    checkLastCommit
     heroku container:login
     ;;
   "build")
+    checkLastCommit
     make PROJECT=production_paperboy build
     ;;
   "build-heroku")
+    checkLastCommit
     make build-heroku
     ;;
   "release")
+    checkLastCommit
     make release
     ;;
   "migrate")
+    checkLastCommit
     heroku run rake db:migrate
     ;;
   "populate")
+    checkLastCommit
     heroku run rake populate:all
     ;;
   "build-local")
