9a20d664c4451c3a355833e137a6ba035bbd6c1d
darthjee <darthjee@gmail.com>
2021-10-25 21:47:02 +0200

    Add paperboy route

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#show', as: :home
   get '/paperboy.user' => 'paperboy#user_script', as: :user_script, defaults: { format: :js }
+  get '/paperboy' => 'paperboy#show', as: :paperboy, defaults: { format: :js }
 
   resources :users, only: [:index] do
     collection do
