b5890e747f4578f19e18afa2459cc3c129c22d01
darthjee <darthjee@gmail.com>
2021-10-25 21:27:51 +0200

    Add base url to script

--- source/app/controllers/user_script_controller.rb
+++ source/app/controllers/user_script_controller.rb
@@ -4,6 +4,15 @@ class UserScriptController < ApplicationController
    protect_from_forgery except: :show
 
   def show
+    render :show, locals: {
+      base_url: base_url
+    }
+  end
+
+  private
+
+  def base_url
+    request.protocol + request.host_with_port
   end
 end
 
