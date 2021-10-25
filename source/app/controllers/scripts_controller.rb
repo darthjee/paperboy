56df735f7bfce71c9788308057795f96e87e88ec
Favini <darthjee@gmail.com>
2021-10-25 23:29:02 +0200

    Userscript (#3)

---
+++ source/app/controllers/scripts_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ScriptsController < ApplicationController
+  include OnePageApplication
+
+  protect_from_forgery except: %i[index content]
+
+  def index
+  end
+
+  def content
+  end
+end
