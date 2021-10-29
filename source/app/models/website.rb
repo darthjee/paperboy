8ccd4ce2ecfd37794f6388126a2ed21ec3469e03
darthjee <darthjee@gmail.com>
2021-10-29 11:51:30 +0200

    Add website model

--- source/app/models/website.rb
+++ source/app/models/website.rb
@@ -2,13 +2,16 @@
 
 class Website < ApplicationRecord
   PROTOCOLS = %w[http https]
+  MAX_PORT  = 2**16 - 1
 
   validates_presence_of :name, :domain
   validates :port,
             numericality: {
-              greater_than_or_equal_to: 0,
               only_integer: true
+            },
+            inclusion: {
+              in: (0..MAX_PORT)
             }
-  validates_inclusion_of :protocol, in: PROTOCOLS
+            validates_inclusion_of :protocol, in: PROTOCOLS
 end
 
