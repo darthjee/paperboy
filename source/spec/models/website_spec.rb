7bfdd8c0799891b43f780e1be2fc8e8677aaeae9
darthjee <darthjee@gmail.com>
2021-10-30 22:43:09 +0200

    Improve specs

--- source/spec/models/website_spec.rb
+++ source/spec/models/website_spec.rb
@@ -14,5 +14,37 @@ describe Website, type: :model do
       expect(website).to validate_length_of(:name)
         .is_at_most(100)
     end
+
+    it do
+      expect(website).to validate_presence_of(:domain)
+    end
+
+    it do
+      expect(website).to validate_length_of(:domain)
+        .is_at_most(100)
+    end
+
+    it do
+      expect(website).not_to validate_presence_of(:port)
+    end
+
+    it do
+      expect(website).to validate_numericality_of(:port)
+        .is_greater_than(0)
+    end
+
+    it do
+      expect(website).to validate_numericality_of(:port)
+        .is_less_than_or_equal_to(described_class::MAX_PORT)
+    end
+
+    it do
+      expect(website).not_to validate_presence_of(:protocol)
+    end
+
+    it do
+      expect(website).to validate_inclusion_of(:protocol)
+        .in_array(described_class::PROTOCOLS)
+    end
   end
 end
