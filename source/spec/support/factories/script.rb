042153175c78c28d233759d9cf87997bdf2b6a91
darthjee <darthjee@gmail.com>
2021-11-03 22:20:47 +0100

    Add factory traits

--- source/spec/support/factories/script.rb
+++ source/spec/support/factories/script.rb
@@ -5,4 +5,14 @@ FactoryBot.define do
     sequence(:name) { |n| "Script ###{n}" }
     external_url    { 'http://www.scriptserver.com' }
   end
+
+  trait :external_url do
+    external_url { 'http://www.scriptserver.com' }
+    content      { nil }
+  end
+
+  trait :content do
+    external_url { nil }
+    content      { 'alert("hello");' }
+  end
 end
