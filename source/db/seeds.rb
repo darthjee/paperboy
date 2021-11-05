2d3516042fa7ca1630d9df5d9f56f74392cfae59
darthjee <darthjee@gmail.com>
2021-11-05 01:25:59 +0100

    Add seed

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -4,3 +4,24 @@
 # The data can then be loaded with the rails db:seed
 # command (or created alongside the database with db:setup).
 #
+
+Website.create(
+  name: 'Localhost',
+  domain: 'localhost',
+  port: 3000
+)
+
+Website.create(
+  name: 'google',
+  domain: 'google.com'
+)
+
+Script.create(
+  name: 'hello',
+  content: 'alert("hello");'
+)
+
+Script.create(
+  name: 'question',
+  content: 'alert("Who am I?");'
+)
