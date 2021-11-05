fd0883abb4d4d8ceb6c9936df6a0c29f6f8fdf96
darthjee <darthjee@gmail.com>
2021-11-05 03:08:25 +0100

    Add textarea

---
+++ source/app/models/magicka/textarea.rb
@@ -0,0 +1,6 @@
+module Magicka
+  # Input element representing an HTML +<textarea>+
+  class Textarea < FormElement
+    with_attribute_locals :placeholder
+  end
+end
--- source/config/initializers/magicka.rb
+++ source/config/initializers/magicka.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 Magicka::Display.with_element(Magicka::NgPagination, :pagination)
+Magicka::Display.with_element(Magicka::Textarea, :textarea)
