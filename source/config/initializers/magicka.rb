d6e229a70548be68aba1b60def45cb286fbbadaf
darthjee <darthjee@gmail.com>
2021-11-08 11:16:08 +0100

    Button is always rendered

--- source/config/initializers/magicka.rb
+++ source/config/initializers/magicka.rb
@@ -3,3 +3,11 @@
 Magicka::Display.with_element(Magicka::NgPagination, :pagination)
 Magicka::Display.with_element(Magicka::Content, :textarea)
 Magicka::Form.with_element(Magicka::Textarea, :textarea)
+
+module Magicka
+  class Display < Aggregator
+    def button(**args)
+      Button.render(renderer: renderer, **args)
+    end
+  end
+end
