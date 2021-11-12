55d1c463f03abf4da3b1f3768cddefaaf9f22fa7
darthjee <darthjee@gmail.com>
2021-11-12 11:34:17 +0100

    Add website scripts

---
+++ source/db/migrate/20211112102101_create_website_scripts.rb
@@ -0,0 +1,15 @@
+class CreateWebsiteScripts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :website_scripts do |t|
+      t.bigint :website_id, null: false
+      t.bigint :script_id, null: false
+      t.string :path
+
+      t.index %i[script_id website_id], unique: true
+      t.index :website_id
+    end
+
+    add_foreign_key :website_scripts, :websites
+    add_foreign_key :website_scripts, :scripts
+  end
+end
