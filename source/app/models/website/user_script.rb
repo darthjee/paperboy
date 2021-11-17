e73ac13f92aab9629868448e47a9840bca010f1e
darthjee <darthjee@gmail.com>
2021-11-17 23:30:25 +0100

    Add primary test for location

--- source/app/models/website/user_script.rb
+++ source/app/models/website/user_script.rb
@@ -4,10 +4,15 @@ class Website < ApplicationRecord
   class UserScript
     attr_reader :website
 
-    delegate :scripts, :name, :domain, to: :website
+    delegate :scripts, :name, to: :website
+    delegate :protocol, :domain, :port, to: :website
 
     def initialize(website)
       @website = website
+    end
+
+    def location
+      "#{protocol}://#{domain}:#{port}/*"
     end
   end
 end
--- source/spec/models/website/user_script_spec.rb
+++ source/spec/models/website/user_script_spec.rb
@@ -5,7 +5,12 @@ require 'spec_helper'
 fdescribe Website::UserScript, type: :model do
   subject(:user_script) { described_class.new(website) }
 
-  let(:website) { create(:website) }
+  let(:domain)   { 'localhost' }
+  let(:port)     { 3000 }
+  let(:protocol) { :http }
+  let(:website) do
+    create(:website, domain: domain, port: port, protocol: protocol)
+  end
 
   describe '#name' do
     it 'delegates to website' do
@@ -13,9 +18,36 @@ fdescribe Website::UserScript, type: :model do
     end
   end
 
-  describe '#domain' do
-    it 'delegates to website' do
-      expect(user_script.domain).to eq(website.domain)
+  describe '#location' do
+    context 'when everything has been defined' do
+      it 'returns the full path' do
+        expect(user_script.location).to eq('http://localhost:3000/*')
+      end
+    end
+
+    context 'when protocol is missing' do
+      let(:protocol) {  nil }
+
+      xit 'adds wild cards for protocol' do
+        expect(user_script.location).to eq('*://localhost:3000/*')
+      end
+    end
+
+    context 'when port is missing' do
+      let(:port) { nil }
+
+      xit 'adds wild cards for port' do
+        expect(user_script.location).to eq('http://localhost:*/*')
+      end
+    end
+
+    context 'when only domain has been defined' do
+      let(:protocol) { nil }
+      let(:port)     { nil }
+
+      xit 'adds wild cards for protocol and port' do
+        expect(user_script.location).to eq('*://localhost:*/*')
+      end
     end
   end
 
