2441e80d08f5594831687798f35ffbdbf9406b3c
darthjee <darthjee@gmail.com>
2021-11-17 23:10:37 +0100

    Add spec

---
+++ source/spec/models/website/user_script_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Website::UserScript, type: :model do
+  subject(:user_script) { described_class.new(website) }
+
+  describe '#name' do
+    xit "does something"
+  end
+
+  describe '#domain' do
+    xit "does something"
+  end
+
+  describe '#scripts' do
+    xit 'does somethiong'
+  end
+end
