cb4946bdb87f9e631806b635e27f2bf1b3599d84
darthjee <darthjee@gmail.com>
2024-04-23 07:52:50 -0300

    Configure magicka

--- source/config/initializers/magicka.rb
+++ source/config/initializers/magicka.rb
@@ -3,3 +3,13 @@
 Magicka::Form.with_element('Magicka::NgSelect', :ng_select)
 Magicka::Display.with_element('Magicka::NgSelectText', :ng_select)
 Magicka::Display.with_element('Magicka::NgPagination', :pagination)
+Magicka::Display.with_element('Magicka::Content', :textarea)
+Magicka::Form.with_element('Magicka::Textarea', :textarea)
+
+module Magicka
+  class Display < Aggregator
+    def button(**args)
+      LinkButton.render(renderer: renderer, **args)
+    end
+  end
+end
