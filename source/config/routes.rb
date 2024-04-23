3345b7c440bef4ef3d08cf992fb3b14bfacdd890
darthjee <darthjee@gmail.com>
2024-04-23 07:42:48 -0300

    Add route

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -3,6 +3,17 @@
 Rails.application.routes.draw do
   get '/' => 'home#show', as: :home
 
+  scope controller: :paperboy, path: '/' do
+    get '/paperboy.user' => :user_script, as: :user_script
+    get '/paperboy' => :show, as: :paperboy_script
+  end
+
+  resources :scripts
+  resources :websites do
+    get '/script.user' => :user_script
+  end
+  resources :website_scripts, defaults: { format: :json }, only: [:index]
+
   resources :users, only: [:index] do
     collection do
       resources :login, only: [:create] do
