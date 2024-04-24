16a29e377b9c4cc4768c23c0d5fb64a24b08b585
darthjee <darthjee@gmail.com>
2024-04-24 10:47:26 -0300

    Split matchers

---
+++ source/app/views/paperboy/_matchers.js
@@ -0,0 +1,34 @@
+EqualMatcher = {
+  match(value, target) {
+    return value == target;
+  }
+};
+
+class AsymetricMatcher {
+  constructor(matcher) {
+    this.matcher = matcher;
+  }
+
+  match(value, target) {
+    if (!value || value == '') {
+      return true
+    }
+    return this.matcher.match(value, target);
+  }
+}
+
+class PathMatcher {
+  constructor(value) {
+    value ||= "";
+
+    this.regexp = new RegExp(value.replace("*", ".*"));
+  }
+
+  match(target) {
+    return target.match(this.regexp);
+  }
+}
+
+PathMatcher.match = function(value, target) {
+  return new this(value).match(target);
+}
--- source/app/views/paperboy/_website_script_parser.js
+++ source/app/views/paperboy/_website_script_parser.js
@@ -1,38 +1,3 @@
-EqualMatcher = {
-  match(value, target) {
-    return value == target;
-  }
-};
-
-class AsymetricMatcher {
-  constructor(matcher) {
-    this.matcher = matcher;
-  }
-
-  match(value, target) {
-    if (!value || value == '') {
-      return true
-    }
-    return this.matcher.match(value, target);
-  }
-}
-
-class PathMatcher {
-  constructor(value) {
-    value ||= "";
-
-    this.regexp = new RegExp(value.replace("*", ".*"));
-  }
-
-  match(target) {
-    return target.match(this.regexp);
-  }
-}
-
-PathMatcher.match = function(value, target) {
-  return new this(value).match(target);
-}
-
 var WebsiteScriptParser = {
   matcher: new AsymetricMatcher(EqualMatcher),
   path_matcher: new AsymetricMatcher(new PathMatcher()),
