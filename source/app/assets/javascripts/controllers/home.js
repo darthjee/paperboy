06bd9a54f96f71057a5ab9978fa3d2691f6f9331
darthjee <darthjee@gmail.com>
2024-05-02 13:50:41 -0300

    Style fix

--- source/app/assets/javascripts/controllers/home.js
+++ source/app/assets/javascripts/controllers/home.js
@@ -4,6 +4,6 @@
   ]);
 
   app.controller("Home.Controller", [
-    'cyberhawk_builder', function(builder) { builder.build(this); }
+    "cyberhawk_builder", function(builder) { builder.build(this); }
   ]);
 }(window._, window.angular, window.Cyberhawk));
