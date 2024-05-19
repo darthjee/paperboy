4dd74f1d990231e48124a24050df241b79d23643
darthjee <darthjee@gmail.com>
2024-05-19 15:49:01 -0300

    Extract concern

--- source/app/decorators/website_script/decorator.rb
+++ source/app/decorators/website_script/decorator.rb
@@ -3,8 +3,11 @@
 class WebsiteScript < ApplicationRecord
   class Decorator < ::ModelDecorator
     expose :id
+    expose :location
     expose :script, decorator: Script::IndexDecorator
     expose :path
     expose :website, decorator: Website::Decorator
+
+    include ExposeWebsite
   end
 end
