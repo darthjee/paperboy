6196001ff6491c0821b0122cc7bb8abf84288247
darthjee <darthjee@gmail.com>
2024-05-26 16:23:28 -0300

    Fix spec

--- source/spec/controllers/script/websites_controller_spec.rb
+++ source/spec/controllers/script/websites_controller_spec.rb
@@ -42,12 +42,12 @@ describe Script::WebsitesController do
       end
 
       it 'adds per_page header' do
-        expect(response.headers['per_page']).to eq(10)
+        expect(response.headers['per_page']).to eq(50)
       end
 
       context 'when there are too many website_scripts' do
-        let(:website_scripts_count) { 21 }
-        let(:expected_object) { Script.limit(10) }
+        let(:website_scripts_count) { 51 }
+        let(:expected_object) { WebsiteScript.limit(50) }
 
         it { expect(response).to be_successful }
 
@@ -60,18 +60,18 @@ describe Script::WebsitesController do
         end
 
         it 'adds pages header' do
-          expect(response.headers['pages']).to eq(3)
+          expect(response.headers['pages']).to eq(2)
         end
 
         it 'adds per_page header' do
-          expect(response.headers['per_page']).to eq(10)
+          expect(response.headers['per_page']).to eq(50)
         end
       end
 
       context 'when requesting last page' do
-        let(:website_scripts_count) { 21 }
-        let(:expected_object)   { Script.offset(20) }
-        let(:parameters)        { { page: 3 } }
+        let(:website_scripts_count) { 51 }
+        let(:expected_object)   { WebsiteScript.offset(50) }
+        let(:parameters)        { { page: 2, script_id: script.id } }
 
         it { expect(response).to be_successful }
 
@@ -80,35 +80,17 @@ describe Script::WebsitesController do
         end
 
         it 'adds page header' do
-          expect(response.headers['page']).to eq(3)
+          expect(response.headers['page']).to eq(2)
         end
 
         it 'adds pages header' do
-          expect(response.headers['pages']).to eq(3)
+          expect(response.headers['pages']).to eq(2)
         end
 
         it 'adds per_page header' do
-          expect(response.headers['per_page']).to eq(10)
+          expect(response.headers['per_page']).to eq(50)
         end
       end
-    end
-
-    context 'when requesting html and ajax is true', :cached do
-      before do
-        get :index, params: { format: :html, ajax: true }
-      end
-
-      it { expect(response).to be_successful }
-
-      it { expect(response).to render_template('website_scripts/index') }
-    end
-
-    context 'when requesting html and ajax is false' do
-      before do
-        get :index
-      end
-
-      it { expect(response).to redirect_to('#/website_scripts') }
     end
   end
 end
