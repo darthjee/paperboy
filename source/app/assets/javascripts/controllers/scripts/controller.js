bd531f126102559af174fca28c1c3ae517704600
darthjee <darthjee@gmail.com>
2024-05-26 18:28:17 -0300

    Add custom controller

--- source/app/assets/javascripts/controllers/scripts/controller.js
+++ source/app/assets/javascripts/controllers/scripts/controller.js
@@ -1,16 +1,9 @@
 (function(_, angular, Cyberhawk) {
-  var app = angular.module("scripts/website_controller", [
+  var app = angular.module("scripts/controller", [
     'cyberhawk/builder'
   ]);
 
   var ControllerMethods = {
-    _getPath() {
-      if ( !this.path ) {
-        this.path = this.location.$$path.replace(/(\/edit)?$/, "/websites");
-      }
-      return this.path;
-    },
-
     _buildCallback() {
     }
   };
@@ -18,11 +11,10 @@
   function Controller(builder) {
     builder.buildAndRequest(this, this._buildCallback);
   }
-  Controller.namee = "webites_script"
 
   _.extend(Controller.prototype, ControllerMethods);
 
-  app.controller("Scripts.WebsitesController",[
+  app.controller("Scripts.Controller",[
     'cyberhawk_builder', Controller
   ]);
 }(window._, window.angular, window.Cyberhawk));
