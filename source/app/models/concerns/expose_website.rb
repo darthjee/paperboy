bb38fcafbdf15cae83975177f7b18eb2f6936009
darthjee <darthjee@gmail.com>
2024-05-26 14:51:24 -0300

    Add missing concern

---
+++ source/app/models/concerns/expose_website.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module ExposeWebsite
+  extend ActiveSupport::Concern
+
+  def location
+    "#{protocol}://#{domain}:#{port}/*"
+  end
+
+  private
+
+  def domain
+    website.domain || '*'
+  end
+
+  def protocol
+    website.protocol || '*'
+  end
+
+  def port
+    website.port || '*'
+  end
+end
